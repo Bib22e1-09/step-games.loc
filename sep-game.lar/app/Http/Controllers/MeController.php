cd4ffefdf36ad10a90f9657549563221d44db2bc
Bib22e1-09 <out1a1time@mail.com>
2023-10-20 18:37:12 +0600

    place

--- sep-game.lar/app/Http/Controllers/MeController.php
+++ sep-game.lar/app/Http/Controllers/MeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MeController extends Controller
@@ -9,7 +10,7 @@ class MeController extends Controller
     public function __invoke(Request $request)
     {
         $user=User::find(1);
-        $user->name = $request->('name','NoName');
+        $user->name = $request->get('name','NoName');
         $user->save();
         return 'Hello world! Hello ' . $user->name;
     }
---
+++ sep-game.lar/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $login = $request->get('login');
+        if (!$login) {
+            return "login is required";
+        }
+
+        $password = $request->get('password');
+        if (!$password) {
+            return "password is required";
+        }
+        if (strlen($password)<8) {
+            return "password is weak (min 8 chars)";
+        }
+
+        if (User::where('login', $login)->exists()) {
+            return "login is already exists";
+        }
+        return User::create([
+            'password' => Hash::make($password),
+            'login' => $login,
+        ]);
+    }
+}
